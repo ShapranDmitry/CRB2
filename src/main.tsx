9fa54f37953ea9d5833ba2cf8c88fb928ee4fc29
ShapranDmitry <74539433+ShapranDmitry@users.noreply.github.com>
2026-06-19 21:21:20 +0600

    HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import { TournamentProvider } from './logic';
+import { HashRouter } from 'react-router';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <TournamentProvider>
       <App />
     </TournamentProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
--- src/pages/123.tsx
+++
@@ -1,9 +0,0 @@
-import { useParams } from "react-router";
-import { useTournamentContext } from "../logic"
-import { useEffect, useState } from "react";
-import "../index.css"
-
-export default function grid_page(){
-
-    return()
-}
