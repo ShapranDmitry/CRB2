76b4c417a7b84881065352c88fdc6eb6bd44d466
ShapranDmitry <74539433+ShapranDmitry@users.noreply.github.com>
2026-06-19 17:54:59 +0600

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import { TournamentProvider } from './logic';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <TournamentProvider>
+    <HashRouter>
+      <TournamentProvider>
       <App />
     </TournamentProvider>
-    </BrowserRouter>
+    </HashRouter>
+    
+    
   </React.StrictMode>
 );
